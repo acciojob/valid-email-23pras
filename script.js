bcdcf711b2946793e000448185f3210b4f279b84
Prashant Pal <palprashant465@gmail.com>
2025-03-13 22:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
-  //your JS code here.
+    // Empty string edge case
+    if (!str) return false;
+
+    // Email regex pattern
+    const emailPattern = /^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z]{2,3})+$/;
+    return emailPattern.test(str);
 }
 
 // Do not change the code below.
